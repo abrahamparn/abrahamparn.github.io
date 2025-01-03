4f77ff2a56f2670022c9c3a32523936fb94795c1
abrahamparn <abrahamnaiborhu@gmail.com>
2025-01-03 09:36:34 +0700

    Moving navbar to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -120,88 +120,11 @@ function App() {
 
   return (
     <main className="bg-white_tertiary relative">
-      <header className="absolute w-full z-10" ref={headerRef}>
-        <nav className="w-full flex flex-row justify-between p-5">
-          <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl">Abraham_pn</p>
-          <button className="text-3xl" onClick={handleMenuOnClick}>
-            ☰
-          </button>
-        </nav>
-
-        <div className="navBoxContainer h-[100dvh] w-screen z-20 absolute top-0 left-0 bg-white text-white_tertiary flex flex-col justify-between">
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-          <div className="navBox w-full bg-black_main h-[5%]"></div>
-        </div>
-        <div className="navContent h-[100dvh] w-screen z-30 absolute top-0 left-0 bg-transparent text-white_tertiary p-5 flex flex-col gap-3">
-          <div className="w-full flex flex-row justify-between text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            <p className="navContentText">Abraham_pn</p>
-            <button className="text-3xl navContentText" onClick={handleMenuOnClick}>
-              X
-            </button>
-          </div>
-          <div className="flex flex-col h-full ps-10 justify-between md:flex-row md:justify-around">
-            <div className="flex flex-col justify-evenly text-3xl md:text-4xl lg:text-6xl xl:text-8xl gap-4 md:gap-0 font-bold">
-              {/* Main Navigation Items */}
-              <NavItem text="HOME" href="#home" onHover={handleNavItemHover} />
-              <NavItem text="ABOUT" href="#about" onHover={handleNavItemHover} />
-              <NavItem text="SKILLS" href="#skills" onHover={handleNavItemHover} />
-              <NavItem text="EXPERIENCE" href="#experience" onHover={handleNavItemHover} />
-              <NavItem text="PROJECT" href="#project" onHover={handleNavItemHover} />
-              <NavItem text="CONTACT" href="#contact" onHover={handleNavItemHover} />
-            </div>
-            <div className="navContentTextBorder w-[1px] h-full bg-white_tertiary hidden md:flex"></div>
-            <div className="md:flex md:justify-center md:text-xl lg:text-3xl xl:text-5xl">
-              <div className="navContentTextBorder opacity-0 w-full border-t bg-white self-end md:hidden"></div>
-
-              <div className="flex flex-row justify-evenly md:flex-col md:justify-center gap-2 lg:gap-10 font-bold">
-                <div className="flex justify-center items-center">
-                  <div className="navContentText">ABRAHAM_PN</div>
-                </div>
-                <div className="flex flex-col gap-2 lg:gap-10">
-                  <NavItem
-                    text="INSTAGRAM"
-                    href="https://www.instagram.com/abraham_pn"
-                    onHover={handleNavItemHover}
-                  />
-                  <NavItem
-                    text="GITHUB"
-                    href="https://github.com/abrahamparn/"
-                    onHover={handleNavItemHover}
-                  />
-                  <NavItem
-                    text="LINKEDIN"
-                    href="https://linkedin.com/in/abrahamnaiborhu"
-                    onHover={handleNavItemHover}
-                  />
-                  <NavItem
-                    text="WHATSAPP"
-                    href="https://wa.me/+6281388302150"
-                    onHover={handleNavItemHover}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Navbar
+        headerRef={headerRef}
+        handleMenuOnClick={handleMenuOnClick}
+        handleNavItemHover={handleNavItemHover}
+      />
       <section id="hero"></section>
     </main>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,234 +1,88 @@
-import Css from "./Navbar.module.css";
-
 import React, { useState, useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { useWindowScroll } from "react-use";
-import { HashLink } from "react-router-hash-link";
-import { Link } from "react-router-dom";
-
-export default function Navbar() {
-  const navContainerRef = useRef(null);
-  const [lastScrollY, setLastScrollY] = useState(0);
-  const [isNavVisible, setIsNavVisible] = useState(true);
-
-  const { y: currentScrollY } = useWindowScroll();
-
-  const onEnter = ({ currentTarget }) => {
-    gsap
-      .timeline()
-      .to(currentTarget, { fontSize: "2rem", duration: 0.2 })
-      .to(".active", { backgroundColor: "black", duration: 0.2 });
-  };
-
-  useEffect(() => {
-    if (currentScrollY === 0) {
-      // Topmost position: show navbar without floating-nav
-      setIsNavVisible(true);
-      navContainerRef.current.classList.remove("floating-nav");
-    } else if (currentScrollY > lastScrollY) {
-      // Scrolling down: hide navbar and apply floating-nav
-      setIsNavVisible(false);
-      navContainerRef.current.classList.add("floating-nav");
-    } else if (currentScrollY < lastScrollY) {
-      // Scrolling up: show navbar with floating-nav
-      setIsNavVisible(true);
-      navContainerRef.current.classList.add("floating-nav");
-    }
-
-    setLastScrollY(currentScrollY);
-  }, [currentScrollY, lastScrollY]);
-
-  useEffect(() => {
-    gsap.to(navContainerRef.current, {
-      y: isNavVisible ? 0 : -100,
-      opacity: isNavVisible ? 1 : 0,
-      duration: 0.2,
-    });
-  }, [isNavVisible]);
-
-  const onLeave = ({ currentTarget }) => {
-    gsap
-      .timeline()
-      .to(currentTarget, { fontSize: "1rem", backgroundColor: "black", duration: 0.2 })
-      .to(".active", { backgroundColor: "#FF8A00", duration: 0.2 });
-  };
-
-  const [showNavBarSmall, setShowNavBarSmall] = useState(false);
-  const tl = useRef(null);
-  function showMenu() {
-    console.log("clicked");
-    if (!tl.current) {
-      animate();
-    } else {
-      tl.current.reversed(!tl.current.reversed());
-    }
-  }
-
-  const animate = () => {
-    if (showNavBarSmall === false) {
-      console.log("false");
-      tl.current = gsap.timeline();
-      tl.current
-        .fromTo(
-          ".toBeAnimated",
-          { opacity: 0, zIndex: -1 },
-          { opacity: 1, duration: 0.2, zIndex: 50 }
-        )
-        .from(".insertIntoAnimation", { x: 1000, ease: "power1.out", stagger: 0.1 })
-        .fromTo(
-          ".active",
-          { backgroundColor: "bg-black" },
-          { backgroundColor: "#FF8A00", duration: 0.1 }
-        );
-      setShowNavBarSmall(!showNavBarSmall); // Update the state after the animation
-      // .from('.randomInsert', {x:random(-1000, 0)})
-    } else if (showNavBarSmall === true) {
-      console.log("false");
-      tl.current = gsap.timeline({
-        onComplete: () => {
-          setShowNavBarSmall(!showNavBarSmall); // Update the state after the animation
-        },
-      });
-
-      tl.current
-        .fromTo(
-          ".toBeAnimated",
-          { opacity: 0, zIndex: -1 },
-          { opacity: 1, duration: 0.2, zIndex: 50 }
-        )
-        .from(".insertIntoAnimation", { x: 1000, ease: "power1.out", stagger: 0.1 })
-        .fromTo(
-          ".active",
-          { backgroundColor: "bg-black" },
-          { backgroundColor: "#FF8A00", duration: 0.1 }
-        );
-    }
-  };
-  const xButtonRef = useRef(null);
-
-  const handleOnClickNavLink = () => {
-    let xButton = xButtonRef.current;
-    xButton.click();
-  };
-
+import NavItem from "./NavItem";
+export default function Navbar({ headerRef, handleMenuOnClick, handleNavItemHover }) {
   return (
-    <>
-      <div className=" bg-black font-sen text-bold fixed inset-x-0  z-50" ref={navContainerRef}>
-        <div className="flex justify-between sm:ms-0 sm:me-0 md:ms-4 md:me-4 lg:ms-12 lg:me-12 xl:ms-20 xl:me-20 text-white">
-          <div className="flex items-center p-6">
-            <p className="font-bold text-lg">ABRAHAM P.N.</p>
-          </div>
+    <header className="absolute w-full z-10" ref={headerRef}>
+      <nav className="w-full flex flex-row justify-between p-5">
+        <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl">Abraham_pn</p>
+        <button className="text-3xl" onClick={handleMenuOnClick}>
+          ☰
+        </button>
+      </nav>
 
-          <div className="justify-between hidden w-full md:flex md:w-auto md:order-1">
-            <ul className="flex flex-col md:p-0 md:flex-row m-0 p-0">
-              <li>
-                <a
-                  href="#experience"
-                  className="block items-center p-6 hover:text-yellow_custom rand"
-                >
-                  EXPERIENCE
-                </a>
-              </li>
-              <li>
-                <a href="#projects" className="block items-center p-6 hover:text-yellow_custom">
-                  WORK
-                </a>
-              </li>
-              <li>
-                <a href="#photography" className="block items-center p-6 hover:text-yellow_custom">
-                  PHOTOGRAPHY
-                </a>
-              </li>
-              <li className={`${Css.active} hover:bg-black_lighter `}>
-                <a href="#contact" className="block items-center p-6 hover:text-yellow_custom">
-                  CONTACT
-                </a>
-              </li>
-            </ul>
+      <div className="navBoxContainer h-[100dvh] w-screen z-20 absolute top-0 left-0 bg-white text-white_tertiary flex flex-col justify-between">
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+        <div className="navBox w-full bg-black_main h-[5%]"></div>
+      </div>
+      <div className="navContent h-[100dvh] w-screen z-30 absolute top-0 left-0 bg-transparent text-white_tertiary p-5 flex flex-col gap-3">
+        <div className="w-full flex flex-row justify-between text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          <p className="navContentText">Abraham_pn</p>
+          <button className="text-3xl navContentText" onClick={handleMenuOnClick}>
+            X
+          </button>
+        </div>
+        <div className="flex flex-col h-full ps-10 justify-between md:flex-row md:justify-around">
+          <div className="flex flex-col justify-evenly text-3xl md:text-4xl lg:text-6xl xl:text-8xl gap-4 md:gap-0 font-bold">
+            {/* Main Navigation Items */}
+            <NavItem text="HOME" href="#home" onHover={handleNavItemHover} />
+            <NavItem text="ABOUT" href="#about" onHover={handleNavItemHover} />
+            <NavItem text="SKILLS" href="#skills" onHover={handleNavItemHover} />
+            <NavItem text="EXPERIENCE" href="#experience" onHover={handleNavItemHover} />
+            <NavItem text="PROJECT" href="#project" onHover={handleNavItemHover} />
+            <NavItem text="CONTACT" href="#contact" onHover={handleNavItemHover} />
           </div>
+          <div className="navContentTextBorder w-[1px] h-full bg-white_tertiary hidden md:flex"></div>
+          <div className="md:flex md:justify-center md:text-xl lg:text-3xl xl:text-5xl">
+            <div className="navContentTextBorder opacity-0 w-full border-t bg-white self-end md:hidden"></div>
 
-          <div
-            className="flex items-center justify-between md:hidden p-6 openAnimation"
-            onClick={showMenu}
-          >
-            <span className="text-white text-xl">☰</span>
+            <div className="flex flex-row justify-evenly md:flex-col md:justify-center gap-2 lg:gap-10 font-bold">
+              <div className="flex justify-center items-center">
+                <div className="navContentText">ABRAHAM_PN</div>
+              </div>
+              <div className="flex flex-col gap-2 lg:gap-10">
+                <NavItem
+                  text="INSTAGRAM"
+                  href="https://www.instagram.com/abraham_pn"
+                  onHover={handleNavItemHover}
+                />
+                <NavItem
+                  text="GITHUB"
+                  href="https://github.com/abrahamparn/"
+                  onHover={handleNavItemHover}
+                />
+                <NavItem
+                  text="LINKEDIN"
+                  href="https://linkedin.com/in/abrahamnaiborhu"
+                  onHover={handleNavItemHover}
+                />
+                <NavItem
+                  text="WHATSAPP"
+                  href="https://wa.me/+6281388302150"
+                  onHover={handleNavItemHover}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
-      {/* Navbar for phone */}
-      <div
-        className={`toBeAnimated bg-black flex flex-col font-sen text-bold w-screen fixed top-0 left-0 h-screen  md:hidden ${
-          showNavBarSmall ? " " : "hidden"
-        }`}
-      >
-        <div className="flex flex-row justify-between">
-          <div className="flex items-center p-6">
-            <p className="font-bold text-lg text-yellow_custom">ABRAHAM P.N.</p>
-          </div>
-          <div className="flex items-center justify-between md:hidden p-6" onClick={showMenu}>
-            <span className="text-yellow_custom text-xl" ref={xButtonRef}>
-              ⛌
-            </span>
-          </div>
-        </div>
-        <div className="justify-between  w-full md:flex md:w-auto md:order-1">
-          <ul className="flex flex-col md:p-0 md:flex-row m-0 p-0">
-            <li
-              className={`${showNavBarSmall ? " " : "hidden"} insertIntoAnimation`}
-              onMouseEnter={onEnter}
-              onMouseLeave={onLeave}
-              onClick={handleOnClickNavLink}
-            >
-              <a
-                href="#experience"
-                className="block items-center p-6 hover:text-yellow_custom text-white"
-              >
-                EXPERIENCE
-              </a>
-            </li>
-            <li
-              className={`${showNavBarSmall ? " " : "hidden"} insertIntoAnimation`}
-              onMouseEnter={onEnter}
-              onMouseLeave={onLeave}
-              onClick={handleOnClickNavLink}
-            >
-              <a
-                href="#projects"
-                className="block items-center p-6 hover:text-yellow_custom text-white"
-              >
-                WORK
-              </a>
-            </li>
-            <li
-              className={`${showNavBarSmall ? " " : "hidden"} insertIntoAnimation`}
-              onMouseEnter={onEnter}
-              onMouseLeave={onLeave}
-              onClick={handleOnClickNavLink}
-            >
-              <a
-                href="#photography"
-                className="block items-center p-6 hover:text-yellow_custom text-white"
-              >
-                PHOTOGRAPHY
-              </a>
-            </li>
-            <li
-              className={` hover:bg-black_lighter insertIntoAnimation`}
-              onMouseEnter={onEnter}
-              onMouseLeave={onLeave}
-              onClick={handleOnClickNavLink}
-            >
-              <a
-                href="#contact"
-                className="block items-center p-6 hover:text-yellow_custom text-white"
-              >
-                CONTACT
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
+    </header>
   );
 }
